66faa98b21b419528a9f71070161d8b02ad4db82
BenjaminKozuch <bk1037@nyu.edu>
2016-06-08 08:56:36 -0400

    css, user_controller/helper, error message, testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,36 @@
 class UsersController < ApplicationController
+  
+
   def new
+
+  	@user = User.new
+  	# from here to user view to create action and back to this  action if save didnt work
+
   end
+
+  def show
+  	@user = User.find( params[:id] )
+  	#debugger
+  end
+
+
+  #params is a hash with a key called :user and value {name: "Foo Bar", email: "foo@invalid",
+                 #password: "foo", password_confirmation: "bar"}
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to Torah Illustrations!"	
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
 end
