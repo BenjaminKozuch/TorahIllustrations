2134018c92092ea367533d0a0b5256f93caba21b
BenjaminKozuch <bk1037@nyu.edu>
2016-06-19 22:58:04 -0400

    password resets and email config

---
+++ app/controllers/account_activations_controller.rb
@@ -0,0 +1,17 @@
+class AccountActivationsController < ApplicationController
+
+
+  #When trying to copy paste the act link from the server, take out the http:// and it will work
+  def edit
+    user = User.find_by(email: params[:email])
+    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+      user.activate
+      log_in user
+      flash[:success] = "Account activated!"
+      redirect_to user
+    else
+      flash[:danger] = "Invalid activation link"
+      redirect_to root_url
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
-  root             'static_pages#home'
+  #get 'password_resets/new'
 
+  #get 'password_resets/edit'
+
+  root                'static_pages#home'
+  get    'help'    => 'static_pages#help'
+  get    'about'   => 'static_pages#about'
+  get    'contact' => 'static_pages#contact'
+  get    'signup'  => 'users#new'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
-
- 
-  get 'help'    => 'static_pages#help'
-  get 'about'   => 'static_pages#about'
-  get 'contact' => 'static_pages#contact'
-  get 'signup'  => 'users#new'
-
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 
 end
